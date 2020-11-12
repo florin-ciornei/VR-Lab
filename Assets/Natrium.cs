43a411e6a813892434a9ada12b7e4e6ddc6f2dd2
florin-ciornei <31960645+florin-ciornei@users.noreply.github.com>
2020-11-12 13:33:17 +0100

    Natrium movement & size decrease on water

--- Assets/Natrium.cs
+++ Assets/Natrium.cs
@@ -1,20 +1,39 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Natrium : MonoBehaviour
 {
+    public float maxMoveSpeed = 10;
+    public float force = 10;
+    public float sizeDecreaseSpeed = 5;
     private Rigidbody rb;
+
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private bool onWater = false;
+
+    IEnumerator OnWater()
     {
+        while (transform.localScale.magnitude > 0.15f)
+        {
+            if (rb.velocity.magnitude < maxMoveSpeed)
+            {
+                var forward = transform.forward;
+                forward.y = 0;
+                forward.Normalize();
+                rb.AddForce(forward * force);
+            }
+
+            transform.localScale = Vector3.Lerp(transform.localScale, Vector3.zero, sizeDecreaseSpeed * Time.deltaTime);
+            yield return new WaitForFixedUpdate();
+        }
+
+        Destroy(gameObject);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -22,6 +41,17 @@ public class Natrium : MonoBehaviour
         if (other.gameObject.CompareTag("Water"))
         {
             rb.constraints = RigidbodyConstraints.FreezePositionY;
+            if (!onWater)
+                StartCoroutine(OnWater());
+            onWater = true;
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Flask"))
+        {
+            transform.Rotate(0, Random.Range(0, 360), 0, Space.World);
         }
     }
 }
