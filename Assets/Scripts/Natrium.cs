4a6cc36ea100f821552d2948f063d80d5c0f3c06
florin-ciornei <31960645+florin-ciornei@users.noreply.github.com>
2020-11-15 11:03:54 +0100

    Add natrium to lab room fix other stuff

--- Assets/Scripts/Natrium.cs
+++ Assets/Scripts/Natrium.cs
@@ -19,6 +19,7 @@ public class Natrium : MonoBehaviour
 
     IEnumerator OnWater()
     {
+        GetComponent<ParticleSystem>().Play();
         while (transform.localScale.magnitude > 0.15f)
         {
             if (rb.velocity.magnitude < maxMoveSpeed)
