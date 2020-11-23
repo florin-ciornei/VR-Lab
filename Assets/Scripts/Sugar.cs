1e0bc66cba317d7db34ab17ff4f36336ce0612a1
denisramiros <denisramiros@gmail.com>
2020-11-23 22:05:35 +0100

    Add dehydration of sugar reaction

---
+++ Assets/Scripts/Sugar.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sugar : MonoBehaviour
+{
+    public GameObject wrapper;
+    public Material dehydratedMaterial;
+    public int maxHeight = 20;
+    public float reactionSpeed = 3;
+
+    private Renderer rend;
+
+    void Start() 
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    IEnumerator StartReaction()
+    {
+        yield return new WaitForSeconds(reactionSpeed);
+        rend.material = dehydratedMaterial;
+
+        while (wrapper.transform.localScale.y < maxHeight)
+        {
+            wrapper.transform.localScale = new Vector3(wrapper.transform.localScale.x, wrapper.transform.localScale.y + 0.1f, wrapper.transform.localScale.z);
+            reactionSpeed -= 0.1f;
+            yield return new WaitForSeconds(reactionSpeed);
+        }
+    }
+
+    public void ApplySulfuricAcid()
+    {
+        StartCoroutine(StartReaction());
+    }
+}
---
+++ Assets/Scripts/SulfuricAcid.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SulfuricAcid : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Sugar"))
+        {
+            other.gameObject.GetComponent<Sugar>().ApplySulfuricAcid();
+            Destroy(gameObject);
+        }
+    }
+}
